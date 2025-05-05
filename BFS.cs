bcb26a1f2cea83773868afe88d146ae29b9576e0
GiangKhung <hoanggiang2004tv@gmail.com>
2025-05-05 13:59:18 +0700

    Add files via upload

---
+++ BFS.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFSAI
+{
+    class BFS
+    {
+        private DoThiBFS dt;
+        private Queue<int> q;
+        private Queue<int> q1;
+        private int[] previous;
+        static readonly int NIL = -5;
+        //Hàm khởi tạo BFS
+        public BFS()
+        {
+            dt = new DoThiBFS();
+            q = new Queue<int>();
+            previous = new int[dt.SoDinh];
+
+            for(int i=0; i < dt.SoDinh; i++)
+            {
+                previous[i] = -2;
+            }
+            previous[dt.Start] = NIL;
+            q.Enqueue(dt.Start);
+        }
+        //Hàm tìm kiếm BFS
+        public void timKiemBFS()
+        {
+            while(q.Count > 0)
+            {
+                q1 = new Queue<int>();
+                foreach (var s in q)
+                {
+                    for(int s1 = 0; s1 < dt.SoDinh; s1++)
+                    {
+                        if (dt.MaTran[s, s1] > 0 && previous[s1] == -2)
+                        {
+                            previous[s1] = s;
+                            q1.Enqueue(s1);
+                        }
+                    }
+                }
+                q = q1;
+            }
+        }
+        //Hàm in kết quả BFS
+        public void InDuongDi()
+        {
+            if (previous[dt.Goal] == -2)
+                Console.WriteLine("KHONG tim duoc duong di");
+            else
+            {
+                int current = dt.Goal;
+                System.Console.Write("\nDuong di: ");
+                while(current != dt.Start)
+                {
+                    Console.Write($"{current} <== ");
+                    current = previous[current];
+                }
+                Console.Write($"{dt.Start}");
+            }
+        }
+    }
+}
---
+++ mainBFS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFSAI
+{
+    class mainBFS
+    {
+        static void Main(string[] args)
+        {
+            //Đọc và in ma trận
+            DoThiBFS dt = new DoThiBFS();
+            dt.DocDoThi();
+            dt.InDoThi();
+            
+            //In danh sách các đỉnh kề
+            //dt.inDanhSachKe();
+            
+            //Test kết quả của BFS
+            BFS bfs = new BFS();
+            bfs.timKiemBFS();
+            bfs.InDuongDi();
+            
+            Console.ReadLine();
+        }
+    }
+}
